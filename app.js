d1e020331d1665d18c20be31c2c188ad70db664b
liveniu <54791952+liveniu@users.noreply.github.com>
2019-09-03 10:41:02 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+http.createServer(function(req, res) {
+	var filepath = path.join(__dirname, 'html', req.url)
+
+	if (!fs.existsSync(filepath)) {
+		filepath = path.join(__dirname, 'html', '404.html')
+	}
+	console.log(filepath.toString());
+
+	fs.readFile(filepath, function(err, data) {
+		if (err) {
+			res.setHeader('Content-type', 'text-plain; charset = utf-8');
+			console.log("wrong" + err);
+			res.end('文件不存在 404');
+		} else {
+			res.setHeader('Content-type', mime.getType(filepath));
+			res.end(data)
+		}
+	});
+}).listen(9090, function() {
+	console.log('http://localhost:9090')
+})
---
+++ appache.js
@@ -0,0 +1,74 @@
+var http = require('http');
+var fs = require('fs');
+var mime = require('mime');
+var path = require('path');
+var url = require('url')
+//var test2 = require('./test2.js')
+var querystring = require('querystring')
+
+http.createServer(function(req, res) {
+	//test2
+	var urlobj = (url.parse(req.url))
+	var filename = urlobj.pathname
+	console.log(filename + req.method)
+	var jsonobject = JSON.parse('[{"id":18,"name":"张三"},{"id":21,"name":"李四"}]');
+	// console.log(jsonobject.length)
+	if (filename === '/test2' && req.method === 'GET') {
+		var urlqueryobject = url.parse(req.url.toLowerCase(), true).query
+		var queryint = parseInt(urlqueryobject.id)
+		if (queryint) {
+			for (var i = 0; i < jsonobject.length; i++) {
+				if (jsonobject[i].id == queryint) {
+					//					console.log(JSON.stringify(jsonobject[i].id))
+					var result = '[' + JSON.stringify(jsonobject[i])+']'
+					res.setHeader('Content-Type', 'application/json');
+					res.end(result)
+				}
+			}
+		} else {
+			res.end(JSON.stringify(jsonobject))
+		}
+	} else if (filename === '/test2' && req.method === 'POST') {
+		var poststring = ''
+		req.on('data', (chunk) => {
+			poststring += chunk.toString('utf8')
+		})
+		req.on('end', () => {
+			queryint = parseInt(querystring.parse(poststring).id)
+			console.log(poststring)
+			if (queryint) {
+				for (var i = 0; i < jsonobject.length; i++) {
+					if (jsonobject[i].id == queryint) {
+						console.log(JSON.stringify(jsonobject[i].id))
+						var result = '[' + JSON.stringify(jsonobject[i])+']'
+						console.log(result)
+						// res.setHeader('Content-Type', 'application/json');
+						res.end(result)
+					}
+				}
+			} else {
+				res.end(JSON.stringify(jsonobject))
+			}
+		})
+	}
+
+
+	var publicpath = path.join(__dirname, 'html', req.url)
+	if (req.url == '/') {
+		console.log(publicpath)
+		publicpath = publicpath + 'index.html';
+	}
+
+	fs.readFile(publicpath, function(err, data) {
+		if (err) {
+			console.log('404 not found');
+			res.end('404 not found!')
+		} else {
+			res.setHeader('Content-Type', mime.getType(publicpath));
+			res.end(data);
+		}
+	})
+
+}).listen(8080, function() {
+	console.log('http://localhost:8080 started');
+})
